a12aa9773257f3cb8eebb68aa36abae4bc75b4b8
Ibtehaz Rafid <IbtehazR@nycstudents.net>
2023-04-19 22:32:31 -0400

    uno

--- src/Maximum.java
+++ src/Maximum.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Maximum {
+public class Maximum {
+    public static void main(String[] args) {
+        int[] arr = {7, 4, 5, 3};
+        System.out.println(maximum(arr, 1));
+    }
+
+    public static int maximum(int[] data, int start) {
+        if (start == data.length - 1) {
+            return data[start];
+        }
+        int val = maximum(data, start + 1);
+        if (val > data[start]) {
+            return val;
+        } else {
+            return data[start];
+        }
+    }
 }
